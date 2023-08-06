d0903b72994c9676de9b4d5148f855ff24d145ed
wosys <203659850@qq.com>
2023-08-06 14:04:15 +0800

    [Java] Refactor project modules

---
+++ _05_restfulcrud/src/test/java/org/wintmain/restfulcrud/Springboot04RestfulcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wintmain.restfulcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot04RestfulcrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
